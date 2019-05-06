6b90c06efaab0e6eddb9dc37f959362066daf62a
Spice <yugo.ogura@eure.jp>
2019-05-06 14:02:06 +0900

    update 全てのmemoを取得するセレクタを追加

--- src/modules/selectors.ts
+++ src/modules/selectors.ts
@@ -4,6 +4,10 @@ export const getCurrentMemo = (state: MemosState) => {
   return state.memos[state.currentMemo];
 };
 
+export const getMemos = (state: MemosState) => {
+  return state.memos;
+};
+
 export const getMemoIdList = (state: MemosState) => {
   return state.memos.map(el => {
     return el.id;
