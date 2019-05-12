3ee3ce6be8cf9df33a578c7452023f80352f0c46
Spice <yugo.ogura@eure.jp>
2019-05-12 18:28:36 +0900

    update middlewareにthunkを追加

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
 import memosApp from "./reducers";
 
-const store = createStore(memosApp);
+const store = createStore(memosApp,applyMiddleware(thunk));
 
 export default store;
